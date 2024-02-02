1e50fac6580d070345db12a7af24480a472eedcd
Divyashreey <divyashreeyuvaraj1@gmail.com>
2024-02-02 12:16:46 +0530

    changed

---
+++ netlify/functions/api.js
@@ -0,0 +1,13 @@
+// YOUR_BASE_DIRECTORY/netlify/functions/api.js
+
+const express = require("express");
+const serverless = require("serverless-http");
+
+const api = express();
+
+const router = express.Router();
+router.get("/hello", (req, res) => res.send("Hello World!"));
+
+api.use("/api/", router);
+
+module.exports.handler = serverless(api);
