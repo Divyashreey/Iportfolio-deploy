655ad118fcf15afe9cf9937be05c7bcb014720e8
Divyashreey <divyashreeyuvaraj1@gmail.com>
2024-02-22 11:24:19 +0530

    link changes

--- server.js
+++ server.js
@@ -10,6 +10,7 @@ const port = 3001;
 app.use(cors());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
+
 // POST endpoint for form submission
 app.post('/submit-form', (req, res) => {
   // Extract data from the form submission
@@ -17,34 +18,31 @@ app.post('/submit-form', (req, res) => {
 
   // Check if any of the required fields are missing
   if (!name || !email || !subject || !message) {
-    return res.status(200).json({ success: true, message: 'Emails sent successfully' });
+    return res.status(400).json({ success: false, error: 'Missing required fields' });
   }
-
-  // Assuming you want to use the 'email' field as the user's email
-  const userEmail = email;
 
   // Create a nodemailer transporter
   const transporter = nodemailer.createTransport({
     service: 'gmail',
     auth: {
       user: 'divyashreey10@gmail.com',
-      pass: 'umpq jgai mxqp bmvh' // Use an application-specific password for security
+      pass: 'umpq jgai mxqp bmvh'
     }
   });
 
   // Email options
   const mailOptionsUser = {
     from: 'divyashreey10@gmail.com',
-    to: userEmail,
+    to: email,
     subject: 'Thank you for your message',
     text: `Dear ${name},\n\nThank you for reaching out. I'll get back to you as soon as possible.\n\nBest regards,\nDivyashree Y`
   };
 
   const mailOptionsAdmin = {
-    from: userEmail,
+    from: email,
     to: 'divyashreey10@gmail.com',
     subject: subject,
-    text: `Name: ${name}\nEmail: ${userEmail}\nMessage: ${message}`
+    text: `Name: ${name}\nEmail: ${email}\nMessage: ${message}`
   };
 
   // Send emails
@@ -57,7 +55,7 @@ app.post('/submit-form', (req, res) => {
   transporter.sendMail(mailOptionsAdmin, (errorAdmin, infoAdmin) => {
     if (errorAdmin) {
       console.error('Error sending admin email:', errorAdmin);
-      return res.status(500).json({ success: true, error: 'Internal Server Error' });
+      return res.status(500).json({ success: false, error: 'Internal Server Error' });
     }
 
     console.log('Emails sent successfully');
